9361952c24ecbc42ad349821dafa32016f161a63
Luca407 <0001081825@senaimgaluno.com.br>
2025-02-05 19:54:01 -0300

    SomaNumeros.cs

---
+++ SomaNumeros.cs
@@ -0,0 +1,23 @@
+using System;
+
+ class Program
+    {
+        public static void Main(string[] args)
+        {
+        int soma = 0;
+        int numero;
+
+        Console.WriteLine("Digite números inteiros para somar (digite 0 para sair):");
+        
+        while (true)
+        {
+            numero = int.Parse(Console.ReadLine()); // Lê um número inteiro do usuário
+            if (numero < 0) break; // Se for 0, sai do loop
+            soma += numero; // Adiciona o número à soma
+        }
+
+        Console.WriteLine("A soma dos números é: "+ soma);
+            Console.ReadKey();
+        }
+        
+    }
