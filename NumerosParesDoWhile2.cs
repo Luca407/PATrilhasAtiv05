500a6279f87036dbff7120e61f585da144b3ae3c
Luca407 <0001081825@senaimgaluno.com.br>
2025-02-06 09:27:43 -0300

    NumerosParesDoWhile2.cs

---
+++ NumerosParesDoWhile2.cs
@@ -0,0 +1,22 @@
+using System;
+
+ class Program
+    {
+        public static void Main(string[] args)
+        {
+        int soma = 0;
+        int numero;
+
+        Console.WriteLine("Digite números inteiros para somar (digite um numero negativo para sair):");
+        
+        
+        do{
+            numero = int.Parse(Console.ReadLine()); // Lê um número inteiro do usuário
+            soma += numero; // Adiciona o número à soma
+        }while (numero = 0);
+
+        Console.WriteLine("A soma dos números é: "+ soma);
+            Console.ReadKey();
+        }
+        
+    }
